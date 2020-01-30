5edcd27a55efa3e03cbc6db6324417040cfe0b22
David Van Fleet <dvfleet413@gmail.com>
2020-01-30 21:52:01 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_reader :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def add_song(song)
+    song.artist = self
+  end
+end
