9625cfc46569ecb4f28db003eebb6acb669c9a5e
David Van Fleet <dvfleet413@gmail.com>
2020-01-30 22:05:42 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
   
   def songs
     binding.pry
-    Song.all.select {|song| song.artist = self}
+    Song.all.select {|song| song.artist == self}
   end
   
   def self.find_or_create_by_name(name)
