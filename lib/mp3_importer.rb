54fd9fee44622dc8f6a1ad9f2478d44ed2dee4a6
David Van Fleet <dvfleet413@gmail.com>
2020-01-30 22:44:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,8 +9,7 @@ class MP3Importer
   
   def files
     files= Dir.entries(@path)
-    files.select {|file| 
-    binding.pry
+    files.select {|file| file.end_with?(".mp3")
   end
   
   def import 
